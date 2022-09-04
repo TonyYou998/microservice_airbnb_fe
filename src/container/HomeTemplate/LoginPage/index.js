26f728a097d400f5d21f18ec6bd4b497a7ab4dfc
huutan3621 <huutan3621@gmail.com>
2022-09-04 23:35:39 +0700

    add background to the page

--- src/container/HomeTemplate/LoginPage/index.js
+++ src/container/HomeTemplate/LoginPage/index.js
@@ -4,12 +4,13 @@ import InputPassword from './component/inputPassword'
 import Carousel from './component/buttonSignIn'
 import ButtonSignIn from './component/buttonSignIn';
 
+
 export default function HomePage() {
   return (
     <div className = 'main'>
       <div className = 'sub-main'>
         <div>
-        <h1>Log In An Account</h1>
+        <h1>Login An Account</h1>
           <h3>Sign in an account to enjoy all the services</h3>
           <h3>without any ads for free!</h3>
           <>
