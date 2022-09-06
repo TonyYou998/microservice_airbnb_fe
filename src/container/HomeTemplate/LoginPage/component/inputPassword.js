d29b0c7b7b943db0211e39dac7da0cd38bd5c5ec
huutan3621 <huutan3621@gmail.com>
2022-09-06 21:28:54 +0700

    update password css and password type

--- src/container/HomeTemplate/LoginPage/component/inputPassword.js
+++ src/container/HomeTemplate/LoginPage/component/inputPassword.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function InputPassword(){
     return(
         <div className='input-password'>
-            <input type ="text" placeholder="Password" className="password input"></input>
+            <input type ="password" placeholder="Password" className="password input"></input>
         </div>
     )
 }
