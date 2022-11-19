17bd2fa7fbb12550140fed704ceacb29d763a8d0
datuio093 <datthanhphan21@gmail.com>
2022-11-19 21:17:33 +0700

    UI

--- src/container/HomeTemplate/DetailPropertyPage/Components/FooterHomePage.js
+++ src/container/HomeTemplate/DetailPropertyPage/Components/FooterHomePage.js
@@ -7,7 +7,7 @@ export default function FooterHomePage() {
 
 
     <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
-    <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom container'>
+    <section className='footer_home d-flex justify-content-center justify-content-lg-between p-4 border-bottom container'>
       <div className='me-5 d-none d-lg-block'>
         <span>Get connected with us on social networks:</span>
       </div>
