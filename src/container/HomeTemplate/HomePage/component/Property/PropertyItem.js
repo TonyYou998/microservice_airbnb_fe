090f1f17b808d2f32a1349d76bf7b15a6c8262f9
tonyyou998 <19520931@gm.uit.edu.vn>
2022-11-23 00:55:38 +0700

    add img in home

--- src/container/HomeTemplate/HomePage/component/Property/PropertyItem.js
+++ src/container/HomeTemplate/HomePage/component/Property/PropertyItem.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function PropertyItem(props) {
   const{info}=props;
-  console.log(info);
+ 
   return (
     <div className='item__container '>
-        <img className='item__img' src='https://a0.muscache.com/im/pictures/8e3c4402-e52b-4d98-9646-ab38f9af7e43.jpg?im_w=720'/>
+        <img className='item__img' src={info&&info.images}/>
         <div className='item__content'>
 
             <h6 className='property__name'>{info&&info.propertyName}</h6>
