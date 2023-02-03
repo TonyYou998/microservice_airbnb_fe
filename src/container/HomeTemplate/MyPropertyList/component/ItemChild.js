c661cf573152af294568a8a127f10fe4eb33cb27
datuio093 <datthanhphan21@gmail.com>
2023-02-03 17:09:39 +0700

    ui list boking user

--- src/container/HomeTemplate/MyPropertyList/component/ItemChild.js
+++ src/container/HomeTemplate/MyPropertyList/component/ItemChild.js
@@ -11,14 +11,14 @@ export default function ItemChild() {
         <img width={240} height={130} src='https://th.bing.com/th/id/R.9e6636a3531d504311061164455c64c8?rik=oKC6LlRm2wn8vw&pid=ImgRaw&r=0'></img> 
         </Col>
         <Col className="info_room ">
-        <p class="card-text"> Tên : xxx </p>
-        <p class="card-text"> Địa Chỉ :  xxx </p>
-        <p class="card-text"> Mô Tả :  xxx. </p>
+        <p class="card-text"> <i className="strong_text"> Tên :  </i> xxx </p>
+        <p class="card-text"> <i className="strong_text"> Địa Chỉ :  </i>  xxx </p>
+        <p class="card-text"> <i className="strong_text"> Mô Tả :  </i> xxx. </p>
 
         </Col>
         <Col className="info_room ">
      
-        <p class="card-text price text-end"> 500 000VNĐ</p>
+        <p class="card-text price text-end"> <i className="strong_text"> 500 000 VNĐ  </i> </p>
 
         </Col>
      </Row>
