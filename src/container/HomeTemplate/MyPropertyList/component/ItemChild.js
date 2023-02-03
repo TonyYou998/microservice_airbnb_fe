3bfb1033d3a4b48219ff89c5cbcce2b50451020e
datuio093 <datthanhphan21@gmail.com>
2023-02-03 15:59:48 +0700

    border..

--- src/container/HomeTemplate/MyPropertyList/component/ItemChild.js
+++ src/container/HomeTemplate/MyPropertyList/component/ItemChild.js
@@ -8,12 +8,12 @@ export default function ItemChild() {
       <div className="table_list">
      <Row>
         <Col className="image_room">
-        <img width={220} height={120} src='https://th.bing.com/th/id/R.9e6636a3531d504311061164455c64c8?rik=oKC6LlRm2wn8vw&pid=ImgRaw&r=0'></img> 
+        <img width={240} height={130} src='https://th.bing.com/th/id/R.9e6636a3531d504311061164455c64c8?rik=oKC6LlRm2wn8vw&pid=ImgRaw&r=0'></img> 
         </Col>
         <Col className="info_room ">
-        <p class="card-text"> Tên : </p>
-        <p class="card-text"> Địa Chỉ : </p>
-        <p class="card-text"> Mô Tả :ntent.</p>
+        <p class="card-text"> Tên : xxx </p>
+        <p class="card-text"> Địa Chỉ :  xxx </p>
+        <p class="card-text"> Mô Tả :  xxx. </p>
 
         </Col>
         <Col className="info_room ">
